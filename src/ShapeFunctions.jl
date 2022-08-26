15268c14d619cbc1a48b1c2cd6f1b13269e03887
cmhamel <cmhamel32@gmail.com>
2022-08-26 16:45:21 -0400

    some more shape function testing and some docstrings in that module

--- src/ShapeFunctions.jl
+++ src/ShapeFunctions.jl
@@ -6,31 +6,61 @@ export J
 export detJ
 export map_∇φ_ξ
 
+using LinearAlgebra
+
+"""
+    AbstractShapeFunction
+Parent type of all shape functions.
+"""
 abstract type AbstractShapeFunction end
 
+"""
+    LagrangeShapeFunction
+Container for shape function methods on the master element.
+"""
 struct LagrangeShapeFunction <: AbstractShapeFunction
     φ::Function
     ∇φ::Function
 end
 
+"""
+    LagrangeShapeFunction(element_type::String)
+Init method for LagrangeShapeFunction.
+"""
 function LagrangeShapeFunction(element_type::String)
     values_method_name = Symbol(lowercase(element_type) * "_shape_function_values")
     grads_method_name = Symbol(lowercase(element_type) * "_shape_function_gradients")
-    
     values_method = getfield(ShapeFunctions, values_method_name)
     grads_method = getfield(ShapeFunctions, grads_method_name)
     return LagrangeShapeFunction(values_method, grads_method)
 end
 
-J(∇φ_ξ::Matrix{T}, X::Matrix{T}) where {T <: Real} = transpose(∇φ_ξ) * X
-# JxW(∇φ_ξ::Matrix{T})
-detJ(∇φ_ξ::Matrix{T}, X::Matrix{T}) where {T <: Real} = det(J(∇φ_ξ, X))
-map_∇φ_ξ(∇φ_ξ::Matrix{T}, X::Matrix{T}) where {T <: Real} = transpose(inv(J(∇φ_ξ, X)) * transpose(∇φ_ξ)) 
+# NEED TO FIGURE OUT TYPE FOR X WITH A VIEW
+"""
+    J(∇φ_ξ::Matrix{T}, X::S) where {T <: Real, S}
+Mapping Jacobian. Written to except views of coordinates.
+"""
+J(∇φ_ξ::Matrix{T}, X::S) where {T <: Real, S} = transpose(∇φ_ξ) * X
+"""
+    detJ(∇φ_ξ::Matrix{T}, X::S) where {T <: Real, S}
+Determinant of mapping Jacobian. Written to except views of coordinates
+"""
+detJ(∇φ_ξ::Matrix{T}, X::S) where {T <: Real, S} = det(J(∇φ_ξ, X))
+"""
+    map_∇φ_ξ(∇φ_ξ::Matrix{T}, X::S) where {T <: Real, S}
+Method to map master element shape function grads to physical element.
+    Written to except views.
+"""
+map_∇φ_ξ(∇φ_ξ::Matrix{T}, X::S) where {T <: Real, S} = transpose(inv(J(∇φ_ξ, X)) * transpose(∇φ_ξ)) 
 
 
 # actual element implementations below
 #
 #
+"""
+    quad4_shape_function_values(ξ::Vector{T}) where {T <: Real}
+Implementation for quad4 element.
+"""
 function quad4_shape_function_values(ξ::Vector{T}) where {T <: Real}
     φ = zeros(T, 4)
     φ[1] = 0.25 * (1.0 - ξ[1]) * (1.0 - ξ[2])
@@ -40,6 +70,10 @@ function quad4_shape_function_values(ξ::Vector{T}) where {T <: Real}
     return φ
 end
 
+"""
+    quad4_shape_function_gradients
+Implementation for quad4 element.
+"""
 function quad4_shape_function_gradients(ξ::Vector{T}) where {T <: Real}
     ∇φ_ξ = zeros(T, 4, 2)
     ∇φ_ξ[1, 1] = -0.25 * (1.0 - ξ[2])
--- test/TestShapeFunctions.jl
+++ test/TestShapeFunctions.jl
@@ -68,3 +68,55 @@ end
     @test ∇φ[4, 1] ≈ -0.25 * (1. - sqrt(1. / 3.))
     @test ∇φ[4, 2] ≈ 0.25 * (1. + sqrt(1. / 3.))
 end
+
+@tardigrade_test_set "ShapeFunctions.jl - Quad4 Order 1 Mapping" begin
+    mesh = SingleBlockExodusMesh("./mesh/mesh_test_1.g")
+    X = element_level_coordinates(mesh, 1)
+    shape_functions = LagrangeShapeFunction("quad4")
+    quadrature = Quadrature("quad4", 1)
+    ∇φ_ξ = shape_functions.∇φ(quadrature.ξ[1, :])
+    ∇φ_X = map_∇φ_ξ(∇φ_ξ, X)
+    J_e = J(∇φ_ξ, X)
+    detJ_e = detJ(∇φ_ξ, X)
+    @test detJ_e ≈ 0.25
+end
+
+@tardigrade_test_set "ShapeFunctions.jl - Quad4 Order 2 Mapping" begin
+    mesh = SingleBlockExodusMesh("./mesh/mesh_test_1.g")
+    X = element_level_coordinates(mesh, 1)
+    shape_functions = LagrangeShapeFunction("quad4")
+    quadrature = Quadrature("quad4", 2)
+
+    for (ξ, w) in quadrature
+        ∇φ_ξ = shape_functions.∇φ(ξ)
+        ∇φ_X = map_∇φ_ξ(∇φ_ξ, X)
+        J_e = J(∇φ_ξ, X)
+        det_J_e = detJ(∇φ_ξ, X)
+        # @show ∇φ_X
+        # @show J_e
+        # @show det_J_e
+
+        @test det_J_e ≈ 0.25
+    end
+end
+
+# @tardigrade_test_set "ShapeFunctions.jl - Quad4 Order 1 Mapping" begin
+#     mesh = SingleBlockExodusMesh("./mesh/mesh_test_0.0078125.g")
+#     # X = element_level_coordinates(mesh, 1)
+#     shape_functions = LagrangeShapeFunction("quad4")
+#     quadrature = Quadrature("quad4", 1)
+#     # ∇φ_ξ = shape_functions.∇φ(quadrature.ξ[1, :])
+#     # @show ∇φ_ξ
+#     # @time ∇φ_X = map_∇φ_ξ(∇φ_ξ, X)
+#     # @time ∇φ_X = map_∇φ_ξ(∇φ_ξ, X)
+#     # @show ∇φ_X
+
+#     ∇φ_ξ = shape_functions.∇φ(quadrature.ξ[1, :])
+#     @time begin
+#         for e in 1:size(mesh.connectivity, 1)
+#             @show e
+#             X = element_level_coordinates(mesh, e)
+#             ∇φ_X = map_∇φ_ξ(∇φ_ξ, X)
+#         end
+#     end
+# end
