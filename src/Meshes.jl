5d77d26d689c91fecfceba372082e77741d9adb2
cmhamel <cmhamel32@gmail.com>
2022-08-26 15:03:31 -0400

    documenting Meshes.jl

--- src/Meshes.jl
+++ src/Meshes.jl
@@ -9,29 +9,46 @@ export element_level_coordinates
 
 using Exodus
 
-# building type hierarchy of meshes so if someone
-# wanted a different format they could add it
+"""
+    AbstractMesh
+Parent type for all meshes.
+"""
 abstract type AbstractMesh end
-abstract type ExodusMesh <: AbstractMesh end
+"""
+    AbstractExodusMesh
+Parent type for all different exodus meshes.
+"""
+abstract type AbstractExodusMesh <: AbstractMesh end
 
-struct SingleBlockExodusMesh{I, F} <: AbstractMesh
+"""
+    SingleBlockExodusMesh{I, F}
+Exodus mesh that only has a single block, good for eductational purposes.
+"""
+struct SingleBlockExodusMesh{I, F} <: AbstractExodusMesh
     nodal_coordinates::Matrix{F}
     connectivity::Matrix{I}
     node_sets::Vector{Exodus.NodeSet{I, I}}
-    function SingleBlockExodusMesh(file_name::String)
-        exo = ExodusDatabase(file_name, "r")
-        init = Initialization(exo)
-        nodal_coordinates = Exodus.read_coordinates(exo, init)
-        block_ids = Exodus.read_block_ids(exo, init)
-        if length(block_ids) > 1
-            error("This only supports single block meshes")
-        end
-        block = Exodus.read_blocks(exo, Exodus.read_block_ids(exo, init))[1]
-        conn = convert_connectivity(block)
-        node_sets = Exodus.read_node_sets(exo, Exodus.read_node_set_ids(exo, init))
-        close(exo)
-        return new{Int32, Float64}(nodal_coordinates, conn, node_sets) # TODO dispatch this better
+end
+
+"""
+    SingleBlockExodusMesh
+Exodus mesh that only has a single block.
+# Arguments
+- `file_name::String`: string of absolute path of mesh file name.
+"""
+function SingleBlockExodusMesh(file_name::String)
+    exo = ExodusDatabase(file_name, "r")
+    init = Initialization(exo)
+    nodal_coordinates = Exodus.read_coordinates(exo, init)
+    block_ids = Exodus.read_block_ids(exo, init)
+    if length(block_ids) > 1
+        error("This only supports single block meshes")
     end
+    block = Exodus.read_blocks(exo, Exodus.read_block_ids(exo, init))[1]
+    conn = convert_connectivity(block)
+    node_sets = Exodus.read_node_sets(exo, Exodus.read_node_set_ids(exo, init))
+    close(exo)
+    return SingleBlockExodusMesh{Int32, Float64}(nodal_coordinates, conn, node_sets)
 end
 
 function convert_connectivity!(conn_out::Matrix{I}, block::Exodus.Block{I, I}) where {I <: Exodus.ExoInt}
@@ -47,10 +64,28 @@ function convert_connectivity(block::Exodus.Block{I, I}) where {I <: Exodus.ExoI
 end
 
 
+"""
+    element_level_connectivity
+# Arguments
+- `mesh::SingleBlockExodusMesh`: exodus mesh object
+- `e::T`: element id, can be either Int32 or Int64
+"""
 element_level_connectivity(mesh::SingleBlockExodusMesh, e::T) where {T <: Integer} = @view mesh.connectivity[e, :]
 
 # need this to have block be a different Matrix
+
+"""
+    element_level_coordinates
+# Arguments
+- `mesh::SingleBlockExodusMesh`: exodus mesh object
+"""
 element_level_coordinates(mesh::SingleBlockExodusMesh) = @view mesh.nodal_coordinates[mesh.connectivity, :]
+"""
+    element_level_coordinates
+# Arguments
+- `mesh::SingleBlockExodusMesh`: exodus mesh object
+- `e::T`: element id, can be either Int32 or Int64
+"""
 element_level_coordinates(mesh::SingleBlockExodusMesh, e::T) where {T <: Integer} = @view mesh.nodal_coordinates[element_level_connectivity(mesh, e), :]
 
 end # module
