abaf88eeca377fb649637af53927654092236a25
cmhamel <cmhamel32@gmail.com>
2022-08-27 16:20:16 -0400

    adding tentative boundary condition file

---
+++ src/boundary_conditions/BoundaryConditions.jl
@@ -0,0 +1,41 @@
+module BoundaryConditions
+
+export AbstractBC
+export AbstractDirichletBC
+export ConstantDirichletBC
+export setup_dirichlet_bcs
+
+using ..Meshes
+
+abstract type AbstractBC end
+abstract type AbstractDirichletBC <: AbstractBC end
+
+function update_residual!(R::Vector{F}, bc::AbstractDirichletBC) where {F <: Real}
+
+end
+
+# this is for a dense matrix, stupid but easy to start with
+function update_jacobian!(K::Matrix{F}, bc::AbstractDirichletBC) where {F <: Real}
+
+end
+
+# add sparse matrix
+
+# eventuall move all the different types to different files
+# and keep generic stuff in here
+struct ConstantDirichletBC{I, F} <: AbstractDirichletBC
+    nodes::Vector{I}
+    value::F
+end
+
+function ConstantDirichletBC()
+
+end
+
+function setup_dirichlet_bcs(mesh::AbstractMesh)
+    for node_set in mesh.node_sets
+
+    end 
+end
+
+end # module
